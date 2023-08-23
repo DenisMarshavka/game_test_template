a3a46bb56ff3a09acbc57922c002d29d6463d8b0
Denis RN <denys.marshavka@swansoftwaresolutions.com>
2023-08-23 15:58:38 +0300

    Fixed the logic for the 'Fish' game size

--- lib/src/components/fish_animation.dart
+++ lib/src/components/fish_animation.dart
@@ -1,4 +1,5 @@
 import 'package:flame/components.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../play_session/flame_game/game.dart';
 
@@ -33,11 +34,15 @@ class FishAnimationComponent extends SpriteAnimationComponent
           y += speed * gameRef.joystick.delta.y * dt;
         }
       case JoystickDirection.right:
-        if (x > 0) {
+        if (x > gameRef.size.x) {
+          x += 0;
+        } else if (x > 0) {
           x += speed * gameRef.joystick.delta.x * dt;
         }
       case JoystickDirection.left:
-        if (x > 0) {
+        if (x < 15.w) {
+          x += 0;
+        } else if (x > 0) {
           x += speed * gameRef.joystick.delta.x * dt;
         }
 
